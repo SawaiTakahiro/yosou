407eb99d1a8d14dd57fb4d2bf96318625f300f18
sawai takahiro <narcissis.jiteki+git@gmail.com>
2016-01-08 23:04:37 +0900

    CSVファイルを読み込んでみる
    読み込んでとりあえず表示してみた。
    実際には、レース単位で切り分けたりしてあれこれする。

--- read_csv.rb
+++ read_csv.rb
@@ -9,3 +9,16 @@ require "fileutils"
 require "CSV"
 require "json"
 
+PATH_SORCE_SHUTUBAHYO = "./source/sample_shutubahyo_20160108.csv"
+
+#targetで出力したものはshift_jisだから、utfにしておく
+def read_csv(file_path_csv)
+	data_csv = CSV.read(file_path_csv, encoding: "Shift_JIS:UTF-8")
+	
+	return data_csv
+end
+
+hoge = read_csv(PATH_SORCE_SHUTUBAHYO)
+hoge.each do |a|
+	p a
+end
