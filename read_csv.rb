a507383a1494c2509aacc55a28e0f2fbf4fe2316
sawai takahiro <narcissis.jiteki+git@gmail.com>
2016-01-09 16:03:53 +0900

    出馬表クラス、開催クラスの追加
    よくよく考えてみたら、csvを読んだらその時点で加工したいよな〜となった。
    csvを読み込む=>kaisaiクラス作る
    ってやると中身もいい感じになるようにする。

--- read_csv.rb
+++ read_csv.rb
@@ -37,14 +37,110 @@ class Data_shussouma
 	
 	#てすと用
 	def test
-		#p self
-		p @uma_raceid
+		p self
+		#p @uma_raceid
 		#p @uma_raceid_no_num
 	end
+end
+
+#出馬表
+class Data_shutubahyo
+	def initialize(source)
+		@shutubahyo = Array.new
+		
+		source.each do |raceid, shussouma|
+			#レースIDの方は出走馬自体に含んでいるから足さない。
+			@shutubahyo << shussouma
+		end
+	end
 	
+	def test
+		@shutubahyo.each do |shussouma|
+			p shussouma.uma_name
+		end
+	end
+end
+
+#今日のレースまとめ。１日単位だけど開催ってしておく
+#出馬表をまとめたもの
+class Kaisai
+	attr_reader :list_raceid
 	
+	def initialize(data_csv)
+		@source = to_source(data_csv)
+		@list_raceid = get_list_raceid(@source)
+		
+		@kaisai = Hash.new
+		
+		id = "RX2016010906010212"
+		add_kaisai(id, @source)
+	end
+	
+	#その日に行われるレースidのリストを作る
+	def get_list_raceid(source)
+		temp = Array.new
+		source.each do |id, shussouma|
+			temp << shussouma.uma_raceid_no_num
+		end
+		
+		return temp.uniq
+	end
+	
+	#レースID,出走馬って形のものに
+	#データはとりあえず、出走馬形式にしておく
+	#そのほうが中身を扱いやすいし
+	def to_source(data_csv)
+		temp = Array.new
+		
+		data_csv.each do |record|
+			shussouma = Data_shussouma.new(record)
+			raceid_no_num = shussouma.uma_raceid_no_num
+			
+			temp << [raceid_no_num, shussouma]
+		end
+		
+		return temp
+	end
+	
+	#ソースデータから、そのレースIDの出走馬だけ抜き出す
+	#出走馬クラスを作るために使う
+	def get_temp_source(id, source)
+		temp = Array.new
+		
+		#当該レースIDの馬を抜き出す => レースごと出走馬一覧
+		#この時点では、sourceと同じ。raceid, 出走馬クラスという形
+		temp_source = source.select{|raceid, shussouma| raceid == id}
+		
+		return temp_source
+	end
+	
+	#出馬表クラスを作って、開催に足していく
+	def add_kaisai(id, source)
+		#レースIDに対応した出走馬だけ抜き出して、
+		temp_source = get_temp_source(id, source)
+		fuga = Data_shutubahyo.new(temp_source).test
+		#fuga.test
+	end
+
+	def test
+		@source.each do |id, shussouma|
+			p shussouma.uma_raceid_no_num
+		end
+	end
 	
 end
+
+=begin
+ メモ：覚書
+ csvファイルを読み込む
+ で、それを加工してあれこれしたい
+ 
+ なんか、読み込んだcsvファイル（Array）を開催クラスに投げると、
+ その中でクラスとか作れそうな気がする
+ 
+ 頭の中を整理したほうがいいかも
+=end
+
 
 #targetで出力したものはshift_jisだから、utfにしておく
 def read_csv(file_path_csv)
@@ -53,8 +149,14 @@ def read_csv(file_path_csv)
 	return data_csv
 end
 
+##################################################
 hoge = read_csv(PATH_SORCE_SHUTUBAHYO)
 
+kaisai = Kaisai.new(hoge)
+#puts kaisai.list_raceid
+
+
+=begin
 source = Array.new
 hoge.each do |data|
 	temp = Data_shussouma.new(data)
@@ -64,7 +166,25 @@ hoge.each do |data|
 	source << [temp.uma_raceid_no_num, temp]
 	
 end
-
+=end
+=begin
 source.each do |raceid, shussouma|
 	p raceid + shussouma.uma_name
 end
+=end
+
+=begin
+id = "RX2016010908010211"	#テスト用
+hoge = source.select{|raceid, shussouma| raceid == id}
+
+fuga = Data_shutubahyo.new
+hoge.each do |raceid, shussouma|
+	#shussouma.test
+	#fuga.add_shussouma(shussouma)
+	p shussouma.instance_variables
+	break
+end
+
+#fuga.test
+=end
+
