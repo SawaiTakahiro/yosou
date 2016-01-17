0dc360cde6351cefc500aa1b3d8af094e89075c6
sawai takahiro <narcissis.jiteki+git@gmail.com>
2016-01-17 10:46:47 +0900

    試しに抜き出してみるところまで。

--- pickup_uma.rb
+++ pickup_uma.rb
@@ -14,10 +14,11 @@ require "./read_csv.rb"
 
 #出馬表データから、対戦型予測が50以上の馬だけ抜き出して返す
 #対戦型で50以下はまず用無しなので削っちゃう
-def ashikiri(data_csv)
+#やっぱり、どこで切るか？は引数でもてる方にした方がよさそう
+def ashikiri(data_csv, rate)
 	pickup = data_csv.select do |record|
 		race_num = record[9].to_i
-		race_num >= 50
+		race_num >= rate
 	end
 	
 	return pickup
@@ -28,11 +29,13 @@ end
 PATH_SOURCE_SHUTUBAHYO = "./source/sample_shutubahyo_20160108.csv"
 data_csv = read_csv(PATH_SOURCE_SHUTUBAHYO)
 
-pickup = ashikiri(data_csv)
+pickup = ashikiri(data_csv, 80)
 
 #スコア順に並べたものを別途用意する
 temp = pickup.sort{|a, b| b[9].to_i <=> a[9].to_i}
 
 temp[0, 5].each do |hoge|
-	Data_shussouma.new(hoge)
+	piyo = Data_shussouma.new(hoge)
+	p piyo.uma_text_umamei
+	p piyo.uma_taisen_yosoku
 end
