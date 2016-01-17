52dcdde029b40afb880ab610f2d0d4608ff724ed
sawai takahiro <narcissis.jiteki+git@gmail.com>
2016-01-17 20:28:31 +0900

    入力支援の部分まで作成。
    ブログ記事が前半、後半に分かれていなかったのでその対応も必要か

--- main.rb
+++ main.rb
@@ -10,6 +10,8 @@ require "fileutils"
 require "CSV"
 require "json"
 
+require "readline"	#コピペ用
+
 require "./config.rb"
 
 require "./read_csv.rb"
@@ -17,37 +19,54 @@ require "./yosou.rb"
 require "./blog_text.rb"
 require "./pickup_uma.rb"
 
+#入力支援
+#テキストを、ブログへ貼り付ける単位で、クリップボードに書き込む
+def copy_paste_support(text)
+	#クリップボードに足す
+	system "echo \"#{text}\" | pbcopy"
+	
+	print "キーを押すと進む\n"
+	Readline.readline	#入力待ちする。適当なキーを押すと進む
+end
+
+
 ############################################################
 data_csv = read_csv(PATH_SOURCE_SHUTUBAHYO)
 
 #読み込んだデータを、扱いやすい形に変換
 kaisai = Kaisai.new(data_csv)
 
-#通常予想のテキスト
-#Text_basho形式
+#=begin
+#通常予想のテキスト Text_basho形式
+#コースごとに繰り返す
 blog_text = get_blog_text(kaisai)
 blog_text.each do |key, value|
-	p "*" * 30
-	puts value.text_title
-	#puts value.list_text_race
-	puts value.name_honmei
-	puts value.text_ogp
-	puts TRACKBACKLIST
+	p "記事のタイトル"
+	copy_paste_support(value.text_title)
+	
+	p "ブログの本文"
+	copy_paste_support(value.list_text_race.join)
+	
+	p "タグ"
+	copy_paste_support(value.name_honmei)
+	
+	p "記事の概要"
+	copy_paste_support(value.text_ogp)
+	
+	p "トラックバックの送信先"
+	copy_paste_support(TRACKBACKLIST)
 end
-
-#テスト用。開催ID
-#RX2016010906
-#RX2016010908
-#puts blog_text["RX2016010906"].list_text_race
+#=end
 
 #厳選馬のテキストを作る
 gensen_uma = Gensen_uma.new(data_csv)
-blog_gensen_uma = Array.new
-blog_gensen_uma << gensen_uma.text_title
-blog_gensen_uma << "■対戦型予測スコアでの厳選馬"
-blog_gensen_uma << to_text_gensen_uma(gensen_uma.pickup_list_score)	#対戦型スコア準拠
-blog_gensen_uma << ""
-blog_gensen_uma << "■狙い目の馬"
-blog_gensen_uma << to_text_gensen_uma(gensen_uma.pickup_list_okaidoku)	#オッズが狙い目な馬
-puts blog_gensen_uma
-puts TRACKBACKLIST
+blog_gensen_uma = gensen_uma.blog_gensen_uma
+
+p "記事のタイトル"
+copy_paste_support(gensen_uma.text_title)
+
+p "ブログの本文"
+copy_paste_support(blog_gensen_uma.join)
+
+p "トラックバックの送信先"
+copy_paste_support(TRACKBACKLIST)
