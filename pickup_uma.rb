761af8596d60d5613ea57b0cb5a04ba5e94582c5
sawai takahiro <narcissis.jiteki+git@gmail.com>
2016-01-17 20:03:57 +0900

    厳選馬のテキストを生成する処理は、厳選馬クラスで持つことに。
    厳選馬を生成する時点で、テキストも一緒に作れた。
    厳選馬自体で完結させたほうがスマートかな。

--- pickup_uma.rb
+++ pickup_uma.rb
@@ -4,6 +4,8 @@
  2016/01/16
  厳選馬をピックアップするスクリプト
  出馬表じゃなく、開催単位でデータを探していくはずなので予想とは別ロジックになるはず。
+ 
+ ブログ用テキストを生成する部分もこれでしか使わなそうだったのでまとめた。
 =end
 
 require "fileutils"
@@ -18,7 +20,7 @@ require "./read_csv.rb"
 #いろいんな条件で抽出したshussoumaクラスの配列を持っている
 #配列の種類はいろいろ増やしてみる
 class Gensen_uma
-	attr_reader :pickup_list_score, :pickup_list_okaidoku, :text_title
+	attr_reader :pickup_list_score, :pickup_list_okaidoku, :text_title, :blog_gensen_uma
 	
 	def initialize(data_csv)
 		@text_title 			= get_date(data_csv)
@@ -26,6 +28,9 @@ class Gensen_uma
 		#それぞれの条件でピックアップしていく
 		@pickup_list_score		= pickup_score(data_csv)
 		@pickup_list_okaidoku	= pickup_okaidoku(data_csv)
+		
+		#ブログへの出力よう
+		@blog_gensen_uma		= get_blog_gensen_uma(self)
 	end
 	
 	#日付の取得をする
@@ -131,5 +136,52 @@ class Gensen_uma
 		end
 	
 		return output
+	end
+	
+	#厳選馬をブログの記事用にまとめたもの
+	#配列形式でつなげたテキストにして返すので、必要に応じてjoinして使う
+	def get_blog_gensen_uma(gensen_uma)
+		blog_gensen_uma = Array.new
+		
+		blog_gensen_uma << "■対戦型予測スコアでの厳選馬"
+		blog_gensen_uma << to_text_gensen_uma(gensen_uma.pickup_list_score)	#対戦型スコア準拠
+		blog_gensen_uma << ""
+		
+		blog_gensen_uma << "■狙い目の馬"
+		blog_gensen_uma << to_text_gensen_uma(gensen_uma.pickup_list_okaidoku)	#オッズが狙い目な馬
+		
+		return blog_gensen_uma
+	end
+	
+	#厳選馬を加工する
+	#shussoumaが入った配列を使って、それを記事用のテキストにして返す
+	#表みたいな形の方が見やすそうだったので、簡単なテーブル組に
+	def to_text_gensen_uma(list_uma)
+		#puts list_uma[0i].instance_variables
+		
+		text = Array.new
+		list_uma.each do |shussouma|
+			basho		= shussouma.uma_basho
+			race_num	= shussouma.uma_race_num
+			name		= shussouma.uma_text_umamei.join
+			
+			#勝率とかも入れる
+			taisen_yosoku = shussouma.uma_taisen_yosoku
+			seiseki		= DATA_MINING_INDEX[taisen_yosoku.to_i.to_s]
+			shoritsu	= format("%d.0%", seiseki["shoritsu"] * 100)
+			rentai		= format("%d.0%", seiseki["rentai"] * 100)
+			fukusho		= format("%d.0%", seiseki["fukusho"] * 100)
+			
+			#print basho, race_num,"R ", name, " ",shoritsu," ",rentai," ",fukusho,"\n"
+			#temp_text = basho + format("%02d", race_num) + "R " + name + " 勝率：" + shoritsu + " 複勝率：" + rentai + " 連対率：" + fukusho,"\n"
+			temp_text = "<tr><td>" + basho + format("%02d", race_num) + "R</td><td>" + name + "</td><td>" + shoritsu + "</td><td>" + rentai + "</td><td>" + fukusho,"</td></tr>\n"
+			
+			text << temp_text
+		end
+		
+		#とりあえず、出走馬の名前は200pxで固定に。文字数的には足りるはず
+		header = '<table border="1"><tr><th>レース</th><th width="200px">名前</th><th>勝率</th><th>連対率</th><th>複勝率</th></tr>'
+		footer = '</table>'
+		return header + text.join + footer
 	end
 end
