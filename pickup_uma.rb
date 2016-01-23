820ed70edbf2819333ca0524afc16577c4bb61e2
sawai takahiro <narcissis.jiteki+git@gmail.com>
2016-01-23 13:39:59 +0900

    WIN5用の予想スクリプトを統合
    まあ、勝率でしか見てないけど…

--- pickup_uma.rb
+++ pickup_uma.rb
@@ -11,6 +11,7 @@
 require "fileutils"
 require "CSV"
 require "json"
+require "date"
 
 require "./config.rb"
 
@@ -20,7 +21,7 @@ require "./read_csv.rb"
 #いろいんな条件で抽出したshussoumaクラスの配列を持っている
 #配列の種類はいろいろ増やしてみる
 class Gensen_uma
-	attr_reader :pickup_list_score, :pickup_list_okaidoku, :text_title, :blog_gensen_uma
+	attr_reader :pickup_list_score, :pickup_list_okaidoku, :text_title, :blog_gensen_uma, :pickup_list_win5
 	
 	def initialize(data_csv)
 		@text_title 			= get_date(data_csv)
@@ -28,6 +29,8 @@ class Gensen_uma
 		#それぞれの条件でピックアップしていく
 		@pickup_list_score		= pickup_score(data_csv)
 		@pickup_list_okaidoku	= pickup_okaidoku(data_csv)
+		@pickup_list_win5		= pickup_win5(data_csv)
+		
 		
 		#ブログへの出力よう
 		@blog_gensen_uma		= get_blog_gensen_uma(self)
@@ -138,19 +141,24 @@ class Gensen_uma
 		return output
 	end
 	
-	#win5の処理を差し込む
-	
 	#厳選馬をブログの記事用にまとめたもの
 	#配列形式でつなげたテキストにして返すので、必要に応じてjoinして使う
 	def get_blog_gensen_uma(gensen_uma)
 		blog_gensen_uma = Array.new
 		
-		blog_gensen_uma << "■対戦型予測スコアでの厳選馬"
+		blog_gensen_uma << "■対戦型予測スコアでの厳選馬<br>"
 		blog_gensen_uma << to_text_gensen_uma(gensen_uma.pickup_list_score)	#対戦型スコア準拠
-		blog_gensen_uma << ""
+		blog_gensen_uma << "<br>"
 		
-		blog_gensen_uma << "■狙い目の馬"
+		blog_gensen_uma << "■狙い目の馬<br>"
 		blog_gensen_uma << to_text_gensen_uma(gensen_uma.pickup_list_okaidoku)	#オッズが狙い目な馬
+		blog_gensen_uma << "<br>"
+		
+		#WIN5のある日なら
+		if gensen_uma.pickup_list_win5.length != 0 then
+			blog_gensen_uma << "■午後のレース<br>"
+			blog_gensen_uma << to_text_gensen_uma(gensen_uma.pickup_list_win5)	#主にWIN5用
+		end
 		
 		return blog_gensen_uma
 	end
@@ -186,4 +194,81 @@ class Gensen_uma
 		footer = '</table>'
 		return header + text.join + footer
 	end
+	
+	
+	#YYYYMMDDという形の数字を日付に変換
+	#それが土曜日か返すメソッド
+	#土曜日=win5が無い日なので。その他の曜日で開催している=WIN5があるはず
+	def check_saturday(text_date)
+		
+		#渡されたテキストをdateに変換
+		date = Date.strptime(text_date, "%Y%m%d")
+		
+		#wdayが6だったら土曜日
+		if date.wday == 6 then
+			return true
+			else
+			return false
+		end
+	end
+	
+	#win5対象レースであろう馬からピックアップ
+	def pickup_win5(data_csv)
+		temp_shussouma	= Data_shussouma.new(data_csv[0])
+		raceid_no_num	= temp_shussouma.uma_raceid_no_num
+		
+		#もしそれが土曜日のデータなら、空白を返す
+		flag_saturday = check_saturday(raceid_no_num[2..9])
+		return [] if flag_saturday == true
+		
+		#ダメそうな馬はこの時点で除外。まったく0になることはないはず
+		pickup = ashikiri(data_csv, 50)
+		
+		#１回、開催データを作ってから加工し直す
+		kaisai = Kaisai.new(pickup)
+		
+		#予想するレースを抜き出すところ
+		#２場開催の時は９レースから予想する
+		#違う時は、10レースから予想する
+		if kaisai.list_basho.length == 2 then
+			yosou_count = 4
+			else
+			yosou_count = 3
+		end
+		
+		#実際に取得するところ
+		list_yosou_race = Array.new
+		kaisai.list_basho.each do |basho_id|
+			#場所ごとにやるレース一覧を取得
+			list_basho_betsu = kaisai.list_raceid.select{|raceid| raceid[0..11] == basho_id}
+			
+			#開催するレースの、後ろからyosou_count分取得
+			temp_list = list_basho_betsu[yosou_count * -1, yosou_count]
+			
+			#それをリストに追加。
+			#+=でやると配列をそのまま繋ぎこむみたい
+			list_yosou_race += temp_list
+		end
+		
+		temp = Array.new
+		list_yosou_race.each do |raceid|
+			list_shussouma = kaisai.get_shutubahyo(raceid).shutubahyo
+			
+			#条件に合う馬だけ抜き出して出力する
+			list_shussouma.each do |shussouma|
+				taisen_yosoku = shussouma.uma_taisen_yosoku
+				
+				#勝率せめて15%ほしい
+				shoritsu = DATA_MINING_INDEX[taisen_yosoku.to_i.to_s]["shoritsu"]
+				next if shoritsu < 0.15
+				
+				#残ったものだけリストに追加
+				temp << shussouma
+			end
+			
+		end
+		
+		return temp
+	end
+	
 end
